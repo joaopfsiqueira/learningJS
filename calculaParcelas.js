534ca52c4bf05d6761adec7af2adb721bda466f5
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-11-08 21:13:53 -0300

    feat: Pascal Case

--- calculaParcelas.js
+++ calculaParcelas.js
@@ -1,5 +1,5 @@
 function calculaParcelas (valorTotal, numeroParcelas) {
-    return valorParcelas = valorTotal / numeroParcelas;
+    return valorTotal / numeroParcelas;
 }
 
 console.log(calculaParcelas(1000, 5))
@@ -9,7 +9,15 @@ console.log(calculaParcelas(1000, 5))
 
 
 function calcula_parcelas (valor_total, numero_parcelas) {
-    return valor_parcelas = valor_total / numero_parcelas;
+    return valor_total / numero_parcelas;
 }
 
 console.log(calcula_parcelas(1000, 5))
+
+
+
+function CalculaParcelas (ValorTotal, NumeroParcelas) {
+    return ValorTotal / NumeroParcelas;
+}
+
+console.log(CalculaParcelas(1000, 5))
