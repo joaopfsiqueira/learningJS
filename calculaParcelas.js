4ebcc57d0aedcbe476daf8185bd934c6f898261c
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-11-08 21:11:45 -0300

    feat: mesmo exemplo snake_case

--- calculaParcelas.js
+++ calculaParcelas.js
@@ -3,3 +3,13 @@ function calculaParcelas (valorTotal, numeroParcelas) {
 }
 
 console.log(calculaParcelas(1000, 5))
+
+
+
+
+
+function calcula_parcelas (valor_total, numero_parcelas) {
+    return valor_parcelas = valor_total / numero_parcelas;
+}
+
+console.log(calcula_parcelas(1000, 5))
